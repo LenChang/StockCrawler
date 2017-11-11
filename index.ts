634e185d72304b0a20a039c9e70e3ed9320b723e
Len Chang <lamgo.chang@gmail.com>
2017-11-11 16:20:29 +0800

    - Testing parse5

--- index.ts
+++ index.ts
@@ -1,6 +1,27 @@
 import * as parse5 from 'parse5';
+import * as https from 'https';
 
-const document: any = parse5.
-    parse('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>') as parse5.AST.Default.Document;
+let data: string = '';
 
-console.log(document.childNodes[1].nodeName); //> 'html'
+https.get('https://tw.stock.yahoo.com/q/q?s=6180', (resp) => {
+
+    // A chunk of data has been recieved.
+    resp.on('data', (chunk) => {
+        data += chunk;
+    });
+
+    // The whole response has been received. Print out the result.
+    resp.on('end', () => {
+        const documentFragment: any = parse5.
+            parseFragment('<td></td>') as parse5.AST.Default.Document;
+        // console.log(documentFragment);
+
+
+        // const document: any = parse5.
+        //     parse(data) as parse5.AST.Default.Document;
+        // console.log(document.childNodes[1].nodeName); //> 'html'
+    });
+
+}).on("error", (err) => {
+    console.log("Error: " + err.message);
+});
